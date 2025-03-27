db8b5b007f634f5c20ec0f0a2fd1c8cba22efc43
Testrox1 <ismail@cloutkitchen.net>
2025-03-27 02:54:43 +0100

    Add Render deployment configuration

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // CORS middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.header('Access-Control-Allow-Origin', 'https://didactic-story-frontend.onrender.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     if (req.method === 'OPTIONS') {
